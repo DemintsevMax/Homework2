dab16075405997bbde7a28b565b9fc19458a63c4
Maksim <raymax.spb@gmail.com>
2024-02-01 14:40:31 +0300

    Домашнее задание 2

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        var y = 4;
+        var result1 = dog + y;
+        System.out.println("dog + y = " + result1);
+        var result2 = cat + y;
+        System.out.println("cat + y = " + result2);
+        var result3 = paper + y;
+        System.out.println("paper + y = " + result3);
+        var x=3.5;
+        var result4 = dog - x;
+        System.out.println("dog -x = " + result4);
+        var f=1.6;
+        var result5 = cat - f;
+        System.out.println("cat - f = " + result5);
+        var s=7639;
+        var result6 = paper - s;
+        System.out.println("paper - s = " + result6);
+
+
+    }
+}
